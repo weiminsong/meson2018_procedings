6fb8f39e644eaa4e4d89238e6e4eda3f5a99ab75
songweimin <songweimin@songweimindeMacBook-Air.local>
2018-08-26 09:59:08 +0100

    modify

--- MesonProcPackage2018/compile.sh
+++ MesonProcPackage2018/compile.sh
@@ -1,4 +1,10 @@
-pdflatex MESON2018_WeiminSong_paper.tex
+//latex MESON2018_WeiminSong_paper.tex
+//dvipdfm MESON2018_WeiminSong_paper.dvi
+//bibtex MESON2018_WeiminSong_paper.aux
+//latex MESON2018_WeiminSong_paper.tex
+//dvipdfm MESON2018_WeiminSong_paper.dvi
+//open MESON2018_WeiminSong_paper.pdf
+pdftex MESON2018_WeiminSong_paper.tex
 bibtex MESON2018_WeiminSong_paper.aux
-pdflatex MESON2018_WeiminSong_paper.tex
-evince MESON2018_WeiminSong_paper.pdf
+pdftex MESON2018_WeiminSong_paper.tex
+open MESON2018_WeiminSong_paper.pdf
